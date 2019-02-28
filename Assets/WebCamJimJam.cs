404ba75170904eec252891da6b91f16be1ed547f
Sabio <sabiodesigns7@gmail.com>
2019-02-28 18:43:15 -0500

    **Need NDI Tools for This** Added in NDI/Webcam Controller Monitor
    added in monitor attached to left hand to see live visuals on networked computer via NDI Tools Virtual Input. (may need to disable webcam for expected results)

---
+++ Assets/WebCamJimJam.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebCamJimJam : MonoBehaviour {
+
+    void Start()
+    {
+        WebCamDevice[] devices = WebCamTexture.devices;
+        WebCamTexture webcamTexture = new WebCamTexture();
+
+        if (devices.Length > 0)
+        {
+            Debug.Log(devices[0].name);
+            webcamTexture.deviceName = devices[0].name;
+            webcamTexture.Play();
+        }
+    }
+}
